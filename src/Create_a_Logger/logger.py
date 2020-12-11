d63d3bbeec967a0081bddab8eaec3b46e2cd26ae
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2020-12-11 11:51:36 -0300

    Log file successfully created

---
+++ src/Create_a_Logger/logger.py
@@ -0,0 +1,23 @@
+import logging
+import time
+#<M/D/Y> <H M S AM/PM> <thread number> < file name> <logging level> <message>
+#logging.basicConfig(name='meu_projeto_logger',
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(process)d %(filename)s'
+                           '%(levelname)s %(message)s',
+                    datefmt='%d/%m/%Y %I:%M:%S %p',
+                    filename='log_info.log',
+                    filemode='a')
+#logging.info('Deu ruim alguma coisa')
+
+LOG = logging.getLogger('meu_projeto_logger')
+LOG.info('this is coming from my new logger')
+LOG.error('this is an error')
+LOG.warning('this is warning')
+
+            
+
+
+
+
+                    
